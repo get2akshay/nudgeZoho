90e2aa26ae4ae6c1cd121589c92a9f09a309bc6f
Akshay Sharma <akshaysharma@Akshays-MBP.Dlink>
2024-01-23 11:55:23 +0530

    json

--- zoho.py
+++ zoho.py
@@ -10,7 +10,7 @@ expires_in = 0
 clientId = "1000.PPKI153U5EWZGDF9Z3LAQXKI3OA8GH"
 clientSecret = "1a77f2b194027d1b35f0f73494c90b8965138d0307"
 # 1000.39c2ea305b1b4f4fc8169850bdcd3b8c.cd63686c26bca7701cc6d9faa6ccce29
-code = "1000.07ad513407deeec6aef16551370fd82b.ce254ba1f21415a49a5233b15b0cb279"
+code = "1000.4e0d6f8643b89f1e8d3955e0797ae25d.9e661f2dc81ba8df218b1bfd847a122e"
 # set the request URL and parameters for token
 token_url = "https://accounts.zoho.in/oauth/v2/token"
 
