7bda1879250ba82714c0b4c3e982cb7752d14e77
Akshay Sharma <akshaysharma@Akshays-MBP.Dlink>
2024-01-25 12:13:54 +0530

    catch empty list

--- zoho.py
+++ zoho.py
@@ -12,7 +12,7 @@ expires_in = 0
 clientId = "1000.PPKI153U5EWZGDF9Z3LAQXKI3OA8GH"
 clientSecret = "1a77f2b194027d1b35f0f73494c90b8965138d0307"
 # 1000.39c2ea305b1b4f4fc8169850bdcd3b8c.cd63686c26bca7701cc6d9faa6ccce29
-code = "1000.ca1fb767c2328c933b75f16dfc5fbef4.0849298e7f2b0db5652d528c311b1012"
+code = "1000.0664a0d2f7102498d890235c54b1e323.e02e0e027a6063181a264319c21b646e"
 # set the request URL and parameters for token
 token_url = "https://accounts.zoho.in/oauth/v2/token"
 
