776e18123e7edfe15c38edd0a2d7b23d06aca31e
Akshay Sharma <akshay.sharma@byplayit.com>
2024-02-06 23:16:03 +0530

    Moved to markin checkin per day

--- sheetsGoogle.py
+++ sheetsGoogle.py
@@ -135,7 +135,7 @@ def workHourRecord(name, mac, YYYY, MM, DD, HH, shift_hours):
             print(f"Time stamp empty for {name} with {mac} in the period {start_time} to {end_time} !")
 
 
-record = workHourRecord("Rajesh", "00:8c:10:30:02:6f", 2023, 12, 1, 9, 15)
+record = sorted(workHourRecord("Rajesh", "00:8c:10:30:02:6f", 2023, 12, 1, 9, 15))
 c = 0
 m = 0
 missingSeconds = 1800
