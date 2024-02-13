0d7108baf2ad954e55aa335844a305da1fa6271a
Akshay Sharma <akshay.sharma@byplayit.com>
2024-02-13 13:27:08 +0530

    Tb fetch

--- tbdevice.py
+++ tbdevice.py
@@ -3,10 +3,62 @@ from lib import tb
 
 api = f"api/tenant/devices"
 # api = "api/device/types"
+# api = "api/device/info"
+
 params = {}
 params.update({"deviceName" : "Entry"})
 t = tb.rest_get(api,params)
 if not t:
     print(f"API {api} resulted Empty json!")
 else:
-    print(t)
+    id = t.get("id").get("id")
+    print(id)
+    api = "api/device/info"
+    params = {}
+    params.update({"deviceId" : id})
+    t = tb.rest_get(api,params)
+    if t.get("active") is not None and t.get("active") is True:
+        print("Active")
+    else
+        print("Inactive")
+
+
+"""{
+  "id": {
+    "entityType": "DEVICE",
+    "id": "587143d0-7c06-11ee-8d04-9bfe3627a1b1"
+  },
+  "createdTime": 1699207719053,
+  "additionalInfo": {
+    "gateway": false,
+    "overwriteActivityTime": false,
+    "description": "Anchor at entry"
+  },
+  "tenantId": {
+    "entityType": "TENANT",
+    "id": "a016a6a0-7bff-11ee-9dc7-1d05a6175b31"
+  },
+  "customerId": {
+    "entityType": "CUSTOMER",
+    "id": "a0959aa0-7bff-11ee-9dc7-1d05a6175b31"
+  },
+  "name": "Entry",
+  "type": "Anchors",
+  "label": "anchor",
+  "deviceProfileId": {
+    "entityType": "DEVICE_PROFILE",
+    "id": "96bff1f0-7c00-11ee-8d04-9bfe3627a1b1"
+  },
+  "deviceData": {
+    "configuration": {
+      "type": "DEFAULT"
+    },
+    "transportConfiguration": {
+      "type": "MQTT"
+    }
+  },
+  "firmwareId": null,
+  "softwareId": null,
+  "externalId": null
+}
+"""
