c6ecedc54a320650f703dfb71871aa4f148c5644
Akshay Sharma <akshay.sharma@byplayit.com>
2024-02-13 11:18:29 +0530

    Tb fetch

--- tbdevice.py
+++ tbdevice.py
@@ -1,4 +1,4 @@
 from lib import tb
 
-t = tb.rest_get("'https://b7d7-103-234-158-46.ngrok-free.app:443/api/tenant/devices?deviceName=Entry")
+t = tb.rest_get("'https://192.168.0.108:8080/api/tenant/devices?deviceName=Entry")
 print(t)
