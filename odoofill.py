cdfbd5cd7edbca4f959b0be175bf8fe505fbc291
Akshay Sharma <akshay.sharma@byplayit.com>
2024-03-06 11:11:40 +0530

    query

--- odoofill.py
+++ odoofill.py
@@ -331,6 +331,6 @@ for mac in employees.values():
     # run_daily(day_attendance, mac, YYYY=2024, MM=2, DD=1, HH=8, test=test)
     # day_attendance(mac, YYYY=2024, MM=2, DD=1, HH=8, test=test)
     # run_daily(dumm_do, mac, YYYY=2024, MM=2, DD=1, HH=8, test=test)
-    run_daily(markinglogic, mac, YYYY=2024, MM=2, DD=1, HH=8, test=test)
+    run_daily(markinglogic, mac, YYYY=2024, MM=3, DD=1, HH=8, test=test)
 
 
