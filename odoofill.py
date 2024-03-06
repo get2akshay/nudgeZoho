1a6e2977decbb36e03cd779bdbf4bef6b18f1763
Akshay Sharma <akshay.sharma@byplayit.com>
2024-03-06 12:28:01 +0530

    query

--- odoofill.py
+++ odoofill.py
@@ -8,7 +8,6 @@ from pdb import set_trace
 import time
 import yaml
 import threading
-
 # offset = (5 * 60 * 60) + (30 * 60)
 offset = (5 * 60 * 60) + (30 * 60)
 tdd = [1706779963, 1706780640, 1706790586, 1706792442, 1706792448, 1706792770, 1706793326, 1706795112, 1706795114, 1706799685, 1706801012, 1706802195, 1706802210, 1706802296, 1706802471, 1706802472, 1706803193, 1706803820, 1706804712, 1706806236, 1706806238, 1706806552, 1706806572, 1706806574, 1706806689, 1706806691, 1706806693, 1706806699, 1706806700, 1706807178, 1706807180, 1706807968, 1706807972, 1706808217, 1706808251, 1706808269, 1706808291, 1706808318, 1706808320, 1706809142, 1706809653, 1706809655, 1706812381, 1706812384, 1706812386, 1706812910]
@@ -124,7 +123,7 @@ def markinglogic(mac, YYYY, MM, DD, HH, test=False):
             time_diff = timestamp - timestamp_list[idx - 1]
             if time_diff > 1800:
                 # More than 30 minutes difference, mark as shift break
-                checkout_thread(timestamp, offset)
+                threading.Thread(target=checkout_thread, args=(timestamp, offset)).start()
                 time.sleep(0.5)
             elif time_diff < 1800:
                 # Less than 30 seconds difference, continue with previous check-in
@@ -133,16 +132,16 @@ def markinglogic(mac, YYYY, MM, DD, HH, test=False):
                 time.sleep(0.1)
                 if not dic.get('id') and not dic.get('check_in'):
                     print("No checkin")
-                    checkin_thread(timestamp, offset)
+                    threading.Thread(target=checkin_thread, args=(timestamp, offset)).start()
                 elif dic.get('id') and dic.get('check_out'):
                     print("checkin but and out both, needs checkin for new timestamp")
-                    checkin_thread(timestamp, offset)
+                    threading.Thread(target=checkin_thread, args=(timestamp, offset)).start()
                 elif dic.get('id') and not dic.get('check_out'):
                     print("checkin but no checkout")
                     continue
             else:
                 # Between 30 seconds and 30 minutes, mark as shift break
-                # to-do mark break
+                # to-do mark break shift
                 print("mark break")
                 continue
 
