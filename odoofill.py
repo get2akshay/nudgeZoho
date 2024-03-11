20383bc63045b20e7858fb932c75d926d4984cd5
Akshay Sharma <akshay.sharma@byplayit.com>
2024-03-11 16:38:31 +0530

    Time offset changes

--- odoofill.py
+++ odoofill.py
@@ -135,7 +135,7 @@ def markinglogic(mac, ist_start_date, test=False):
                 if check_out_str:
                     check_out = odoo.get_epoch_timestamp(check_out_str)
                 if idd and check_in and not check_out and timestamp > check_in:
-                    checkout_thread(mac, timestamp_list[idx - 1], idd)
+                    checkout_thread(timestamp_list[idx - 1], idd)
                     checkin_thread(timestamp)
                 greater = True
                 less = False
