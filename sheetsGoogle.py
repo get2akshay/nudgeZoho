4e42946ed62090e36c7023aab5cbd3f766c5d9cf
Akshay Sharma <akshay.sharma@byplayit.com>
2024-03-21 07:07:22 +0530

    Test false

--- sheetsGoogle.py
+++ sheetsGoogle.py
@@ -307,9 +307,12 @@ def processData(name, mac, ist_start_date, shift_hours=12, missingSeconds=1800):
     offfloor_min = 0
     if offfloor is not None and offfloor > 0:
         offfloor_min = offfloor / 60
-    data_to_add = [name, mac, month, in_date, in_time, out_date, out_time, total_hours, offfloor_min]  # Provide the data to be added to each column
-    addData(data_to_add)
-    return True
+    if in_time and out_time:
+        data_to_add = [name, mac, month, in_date, in_time, out_date, out_time, total_hours, offfloor_min]  # Provide the data to be added to each column
+        addData(data_to_add)
+        return True
+    else:
+        return False
 
 # processData(YYYY=2024, MM=3, DD=1, HH=8, shift_hours=12, missingSeconds=1800, days_in_month=31)
         
